d145c2188f08e7b11c8a53afbc46dea3cf1a1934
Sylvia <635907588@qq.com>
2017-01-25 00:11:26 +0100

    change words

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -3,14 +3,10 @@
 @section('content')
   <div class="jumbotron">
     <h1>Hello Laravel</h1>
-    <p class="lead">
-      你现在所看到的是 <a href="https://laravel-china.org/laravel-tutorial/5.1">Laravel 入门教程</a> 的示例项目主页。
-    </p>
+    This is the main page!
+
     <p>
-      一切，将从这里开始。
-    </p>
-    <p>
-      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">现在注册</a>
+      <a class="btn btn-lg btn-success" href="{{ route('signup') }}" role="button">Sign up now</a>
     </p>
   </div>
 @stop
